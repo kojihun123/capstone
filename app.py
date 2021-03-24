1cb1b42b63becfa0f1c81895c70aaf30829a52ee
kojihun <rhehddl97@gmail.com>
2021-03-24 22:07:38 +0900

    s

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 def index():
     
     #보낼 현제 위치 데이터 (위도, 경도)
-    value = current_loaction.mylocation()
+    #value = current_loaction.mylocation()
 
-    return render_template('index.html', data1=value[0], data2=value[1])
+    return render_template('index.html')
+    #, data1=value[0], data2=value[1]
 
 if __name__ == '__main__':
     app.run(debug=True)
--- current_loaction.py
+++ current_loaction.py
@@ -8,7 +8,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import requests
 
 def mylocation():
 
