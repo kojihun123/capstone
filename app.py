6fcd61c376919ea8f7914a558e23a53450a00b73
kojihun <rhehddl97@gmail.com>
2021-03-24 23:43:35 +0900

    s

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def index():
     
     #보낼 현제 위치 데이터 (위도, 경도)
-    value1, value2 = current_loaction.mylocations()
-
-    return render_template('index.html', data1=value1, data2=value2)
+    #value1, value2 = current_loaction.mylocations()
+    #data1=value1, data2=value2
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
