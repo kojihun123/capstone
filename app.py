2e1da86d17919b162853a3e22ececaf5d749c286
kojihun <rhehddl97@gmail.com>
2021-03-24 21:48:47 +0900

    s

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, request
+import current_loaction
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    #보낼 현제 위치 데이터 (위도, 경도)
+    value = current_loaction.mylocation()
+
+    data = {'위도':value[0], '경도':value[1]}
+
+
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ current_loaction.py
@@ -0,0 +1,37 @@
+# 위도와 경도가 표시되는 구글 맵을 활용해 자신의 현재 위치를 파악하기 위한 모듈
+# 현재위치에 따라 url이 바뀌는 성질을 이용하여
+# 구글 맵 홈페이지에 들어가 자신의 현재 위치를 파악하는 버튼을 누르고 url을 얻어와
+# split을 통해 위도와 경도를 얻어낸다.
+# 셀레니움으로 자동 크롤링 하는 방식을 채택
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import requests
+
+def mylocation():
+    driver = webdriver.Chrome('C:\chromedriver.exe')
+    driver.get(url = "https://www.google.com/maps")
+    driver.implicitly_wait(300)
+
+
+    #웹페이지 로딩으로 인한 오류를 방지하기위한 While + Try-except 문
+    while True:
+        try:
+            driver.find_element_by_css_selector('#widget-mylocation').click()
+            time.sleep(2)
+            result = driver.current_url
+            #위도경도 테스트
+            print(result.split('@')[1].split(',')[0:2])
+            break
+
+        except:
+            print("웹 페이지 로딩")
+
+    #위도와 경도
+    latitude = result.split('@')[1].split(',')[0]
+    longtitude = result.split('@')[1].split(',')[1]
+
+    return latitude, longtitude
+
