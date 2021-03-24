547d3def906f266abff893c4061545cbed8d2e89
kojihun <rhehddl97@gmail.com>
2021-03-24 21:52:52 +0900

    s

--- app.py
+++ app.py
@@ -10,10 +10,7 @@ def index():
     #보낼 현제 위치 데이터 (위도, 경도)
     value = current_loaction.mylocation()
 
-    data = {'위도':value[0], '경도':value[1]}
-
-
-    return render_template('index.html', data=data)
+    return render_template('index.html', data1=value[0], data2=value[1])
 
 if __name__ == '__main__':
     app.run(debug=True)
