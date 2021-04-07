097629773a3fac843c3afbae6728809907899d6c
kojihun <rhehddl97@gmail.com>
2021-04-07 14:36:48 +0900

    e

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/',methods=('GET', 'POST'))
 def index():
     if request.method == "POST":
-        data = request.form.get('real_addr') # 안전하게 가져오려면 get
+        data = request.form.get('hidden_addr') # 안전하게 가져오려면 get
         print(data)
 
         return render_template('index.html')
---
+++ gmap.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import csv
+from geopy import distance
+import numpy as np
+
+data = pd.read_csv("./지역화폐 가맹점 현황.csv", encoding='euc-kr')
+
+# Haversine 기본 거리 공식을 정의 (죄표로 직선거리 계산)
+def haversine(lat1, lon1, lat2, lon2):
+    MILES = 3959
+    lat1, lon1, lat2, lon2 = map(np.deg2rad, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1 
+    dlon = lon2 - lon1 
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a)) 
+    total_miles = MILES * c
+    return total_miles
+
+
+def ret_location(lat, long):
+
+    find_row = data.loc[haversine(lat, long, data['위도'], data['경도']) <= 0.1553428]
+    print(find_row)
+    return find_row
+
