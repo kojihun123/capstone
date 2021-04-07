389da7d039061fc4a0465605a0556791f873ee2d
kojihun <rhehddl97@gmail.com>
2021-04-07 21:45:47 +0900

    s

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 @app.route('/',methods=('GET', 'POST'))
 def index():
     if request.method == "POST":
-        #data = request.form.get('hidden_addr') # 안전하게 가져오려면 get
-        #a = data.split(',')[0].lstrip("(")
-        #b = data.split(',')[1].lstrip(" ").rstrip(")")
-        loc_data = gmap.ret_location(37.198498, 127.048975)
+        data = request.form.get('hidden_addr') # 안전하게 가져오려면 get
+        a = data.split(',')[0].lstrip("(")
+        b = data.split(',')[1].lstrip(" ").rstrip(")")
+        loc_data = gmap.ret_location(float(a), float(b))
         return render_template('index.html', mydata=loc_data)
 
     elif request.method == "GET":
@@ -18,4 +18,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
