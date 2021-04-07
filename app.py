0b7c7dc04d3aa854aab4a670ab3bd3f87c09be18
kojihun <rhehddl97@gmail.com>
2021-04-07 16:44:15 +0900

    e

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ def index():
     if request.method == "POST":
         data = request.form.get('hidden_addr') # 안전하게 가져오려면 get
         print(data)
-        loc_data = gmap.ret_location(data[0], data[1])
+        print(data[0])
+        # loc_data = gmap.ret_location(data[0], data[1])
 
-        return render_template('index.html', data=loc_data)
+        return render_template('index.html')
 
     elif request.method == "GET":
         user = "반원"
