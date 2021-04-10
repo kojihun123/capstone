7119c029c9317aedc6a00d52a73af8c401142914
kojihun <rhehddl97@gmail.com>
2021-04-10 21:52:17 +0900

    s

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ def index():
 @app.route('/ajax', methods=['POST'])
 def ajax():
     data = request.get_json()
-    print(type(data))
-    print(data)
-    return jsonify(result = "success", result2= data)
+    a = data.split(',')[0].lstrip("(")
+    b = data.split(',')[1].lstrip(" ").rstrip(")")
+    loc_data = gmap.ret_location(float(a), float(b))
+
+    return jsonify(result = "success", mydata= loc_data)
 
 
 if __name__ == '__main__':
